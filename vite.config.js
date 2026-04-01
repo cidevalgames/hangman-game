e5fbe95bd46549e18dd0fa93aefd2698ed218b31
Cideval Games <valleriandicque10@gmail.com>
2026-04-01 17:11:54 +0200

    chore: add gh-pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hangman-game/',
   server: {        // ← tu ajoutes cette partie
     open: true,
     browser: 'chrome'
